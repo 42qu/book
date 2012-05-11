593bc56ad97a0c0c39e750073f33c2fe466c627d
huangz1990 <huangz1990@gmail.com>
2012-05-11 20:15:11 +0800

    为皮肤添加主机AD

--- conf.py
+++ conf.py
@@ -91,7 +91,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'default_with_sidebar_ad'
 
 html_add_permalinks = "#"
 
@@ -106,7 +106,7 @@ html_theme_options = {
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = ['.']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
